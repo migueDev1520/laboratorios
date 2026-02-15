476f42fbe0787f52d66672884c0a29df89b74e76
Luis <luismartins1520@gmail.com>
2026-02-15 23:55:24 +0000

    ejercicioBundling

--- 03-bundling/bundlingProject/vite.config.ts
+++ 03-bundling/bundlingProject/vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from "vite";
 import checker from "vite-plugin-checker";
 import analyzer from "vite-bundle-analyzer";
+import { compression } from "vite-plugin-compression2";
 
 export default defineConfig({
-  plugins: [checker({ typescript: true }), analyzer()],
+  plugins: [
+    checker({ typescript: true }),
+    analyzer(),
+
+    compression({
+      algorithms: ["gzip"],
+      exclude: [/\.(br)$/, /\.(gz)$/],
+    }),
+
+    compression({
+      algorithms: ["brotliCompress"],
+      exclude: [/\.(br)$/, /\.(gz)$/],
+    }),
+  ],
 });
